477148aa46eb2b10600b458eef33388da24163c8
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-04-18 18:08:10 +0200

    Logo goes into the binary

--- golangembed/golangembed.go
+++ golangembed/golangembed.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
 	"fmt"
+	"image"
+	_ "image/png"
 )
 
 //go:embed example.*
 var exampleText string
 
+//go:embed 42SnippetsLogo.png
+var logoBytes []byte
+
 func main() {
 
 	fmt.Println("# 42Snippets")
-	fmt.Println("## Golag Embed")
+	fmt.Println("## Golang Embed")
 
 	/*
 		This Go code demonstrates the use of the embed package to include an external file's contents within the program. By leveraging the //go:embed directive, the content is stored as a string in the exampleText variable. The main function then prints the embedded file's contents, showcasing a simple yet effective use of Go's embed feature.
@@ -19,4 +25,12 @@ func main() {
 
 	fmt.Println("Contents of example.txt:")
 	fmt.Println(exampleText)
+
+	img, _, err := image.Decode(bytes.NewReader(logoBytes))
+	if err != nil {
+		fmt.Println("Error decoding image:", err)
+		return
+	}
+	fmt.Printf("Image size: %d x %d\n", img.Bounds().Dx(), img.Bounds().Dy())
+
 }
