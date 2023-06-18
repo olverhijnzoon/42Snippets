34acb4d7af66985551b578fe6c592eaff831462a
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-06-18 23:59:12 +0200

    Chebyshev distance

--- fortrandistance/geometry.f90
+++ fortrandistance/geometry.f90
@@ -1,7 +1,7 @@
 ! 42Snippets
 ! Fortran Distance
 
-! This Fortran program calculates the Euclidean and Manhattan distances between two points in a 3D space. 
+! This Fortran program calculates the Euclidean, Manhattan and Chebyshev distances between two points in a 3D space. 
 ! It defines a derived type named Point that has three coordinates (x, y, z) 
 ! and functions for distance computation in a module named Geometry. 
 ! These functions compute the Euclidean and Manhattan distances between two points. 
@@ -10,6 +10,7 @@
 
 ! Euclidean Distance: The most common use of distance. In simple terms, it is the shortest distance between two points, and it can be used for all types of data.
 ! Manhattan Distance (L1 norm): Also known as city block distance. It is the distance between two points in a grid-based system (like a chess board or city blocks).
+! Chebyshev Distance (L∞ norm): It is a distance measure between two vectors or points in N-dimensional vector space. It is also known as maximum value distance.
 
 ! Define a module named Geometry
 module Geometry
@@ -34,14 +35,20 @@ contains
     type(Point), intent(in) :: point2
     manhattan_distance = abs(point1%x - point2%x) + abs(point1%y - point2%y) + abs(point1%z - point2%z)
   end function manhattan_distance
+  
+  ! Function to compute Chebyshev distance between two points
+  real function chebyshev_distance(point1, point2)
+    type(Point), intent(in) :: point1
+    type(Point), intent(in) :: point2
+    chebyshev_distance = max(abs(point1%x - point2%x), abs(point1%y - point2%y), abs(point1%z - point2%z))
+  end function chebyshev_distance
 end module Geometry
-
 
 program Main
   use Geometry
   implicit none
   type(Point) :: p1, p2
-  real :: euclidean_dist, manhattan_dist
+  real :: euclidean_dist, manhattan_dist, chebyshev_dist
   
   ! Initialize the random number generator
   call random_seed()
@@ -57,9 +64,11 @@ program Main
   
   euclidean_dist = euclidean_distance(p1, p2)
   manhattan_dist = manhattan_distance(p1, p2)
+  chebyshev_dist = chebyshev_distance(p1, p2)
   
   print*, 'The coordinates of the first point are: ', p1%x, p1%y, p1%z
   print*, 'The coordinates of the second point are: ', p2%x, p2%y, p2%z
   print*, 'The Euclidean distance between points is: ', euclidean_dist
   print*, 'The Manhattan distance between points is: ', manhattan_dist
+  print*, 'The Chebyshev distance between points is: ', chebyshev_dist
 end program Main
