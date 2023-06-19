2ecc5246b9aa20b75179972d459518d1936c0855
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-06-19 15:56:05 +0200

    Draft snippet for monitoring logging and observability in Google Cloud

---
+++ googlecloudoperations/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "The ID of the Google Cloud project."
+  type        = string
+}
+
+variable "email_address" {
+  description = "The email address of the person being notified."
+  type        = string
+}
