a021563fee515660524eea82cb72ed2590a0413d
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-06-20 11:43:21 +0200

    Drafting alerts module

--- googlecloudoperations/main.tf
+++ googlecloudoperations/main.tf
@@ -60,36 +60,9 @@ resource "google_monitoring_uptime_check_config" "default" {
   depends_on = [google_compute_instance.default]
 }
 
-resource "google_monitoring_notification_channel" "default" {
-  display_name = "notification-channel"
-  type         = "email"
-  labels = {
-    email_address = var.email_address
-  }
-}
-
-resource "google_monitoring_alert_policy" "default" {
-  display_name = "alert-policy"
-  combiner     = "OR"
-
-  conditions {
-    display_name = "uptime-check"
-    condition_threshold {
-      filter     = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\""
-      comparison = "COMPARISON_LT"
-      threshold_value = 1
-      duration   = "60s"
-
-      aggregations {
-        alignment_period   = "60s"
-        per_series_aligner = "ALIGN_NEXT_OLDER"
-      }
-    }
-  }
-
-  notification_channels = [
-    google_monitoring_notification_channel.default.name
-  ]
+module "alerts" {
+  source = "./modules/alerts"
+  email_address = var.email_address
 }
 
 module "dashboards" {
