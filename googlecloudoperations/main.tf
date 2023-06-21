76e31fbefa530ae9560339b795757f7953ee2fc6
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-06-21 15:57:32 +0200

    Added servicelevels - changed to Apache2 - installed Ops Agent

--- googlecloudoperations/main.tf
+++ googlecloudoperations/main.tf
@@ -19,10 +19,38 @@ resource "google_storage_bucket" "terraform_state" {
   location = "EU"
 }
 
+resource "google_service_account" "ops_agent" {
+  account_id   = "ops-agent-service-account"
+  display_name = "Service Account for Google Cloud Ops Agent"
+}
+
+resource "google_project_iam_member" "monitoring_writer" {
+  project = var.project
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.ops_agent.email}"
+}
+
+resource "google_project_iam_member" "monitoring_viewer" {
+  project = var.project
+  role    = "roles/monitoring.viewer"
+  member  = "serviceAccount:${google_service_account.ops_agent.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  project = var.project
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.ops_agent.email}"
+}
+
 resource "google_compute_instance" "default" {
   name         = "snippets42"
   machine_type = var.machine_type
-  tags = ["http-server"]
+  tags         = ["http-server"]
+
+  service_account {
+    email  = google_service_account.ops_agent.email
+    scopes = ["cloud-platform"]
+  }
 
   boot_disk {
     initialize_params {
@@ -32,20 +60,49 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
-
     # Enable external IP
     access_config {}
   }
 
   metadata_startup_script = <<SCRIPT
   apt-get update
-  apt-get install -y nginx
-  systemctl start nginx
-  SCRIPT
+  apt-get install -y apache2 php7.0
+  systemctl start apache2
+  curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+  sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+  # Create a configuration file for Ops Agent
+  cat > /etc/google-cloud-ops-agent/config.yaml <<EOL
+  logging:
+    receivers:
+      apache_access:
+        type: apache_access
+      apache_error:
+        type: apache_error
+    service:
+      pipelines:
+        apache:
+          receivers:
+            - apache_access
+            - apache_error
+  metrics:
+    receivers:
+      apache:
+        type: apache
+    service:
+      pipelines:
+        apache:
+          receivers:
+            - apache
+  EOL
+
+  # Restart the Ops Agent to apply the new configuration
+  sudo service google-cloud-ops-agent restart
+SCRIPT
 }
 
 resource "google_compute_firewall" "default" {
-  name    = "nginx-allow-http"
+  name    = "apache-allow-http"
   network = "default"
 
   allow {
@@ -87,4 +144,22 @@ module "alerts" {
 
 module "dashboards" {
   source = "./modules/dashboards"
+}
+
+resource "google_monitoring_custom_service" "apache_service" {
+  service_id = "apache-service"
+  display_name = "Apache Web Server"
+
+  # Define the service's custom resource type.
+  # This example uses 'gce_instance' as the resource type for the Nginx web server.
+  telemetry {
+    resource_name = "gce_instance"
+  }
+}
+
+module "servicelevels" {
+  source      = "./modules/servicelevels"
+  goal        = 0.99 # For example, 99% availability
+  service     = "apache-service"
+  slo_id      = "apache-availability-slo"
 }
--- googlecloudoperations/modules/dashboards/main.tf
+++ googlecloudoperations/modules/dashboards/main.tf
@@ -18,25 +18,35 @@ resource "google_monitoring_dashboard" "dashboard_grid_layout_demo" {
   dashboard_json = <<EOF
 {
   "dashboardFilters": [],
-  "displayName": "Grid Layout Demo",
+  "displayName": "Apache2 Demo Dashboard with Ops Agent Metric",
   "mosaicLayout": {
-    "columns": 12,
+    "columns": 4,
     "tiles": [
       {
-        "height": 4,
+        "height": 2,
         "widget": {
-          "title": "Widget 1",
+          "title": "workload/apache.workers [SUM]",
           "xyChart": {
             "chartOptions": {
               "mode": "COLOR"
             },
             "dataSets": [
               {
+                "minAlignmentPeriod": "60s",
                 "plotType": "LINE",
                 "targetAxis": "Y1",
                 "timeSeriesQuery": {
-                  "timeSeriesQueryLanguage": "fetch gce_instance::compute.googleapis.com/instance/cpu/utilization\n| filter instance_name =~ 'snippets.*'",
-                  "unitOverride": ""
+                  "timeSeriesFilter": {
+                    "aggregation": {
+                      "alignmentPeriod": "60s",
+                      "perSeriesAligner": "ALIGN_SUM"
+                    },
+                    "filter": "metric.type=\"workload.googleapis.com/apache.workers\" resource.type=\"gce_instance\"",
+                    "secondaryAggregation": {
+                      "alignmentPeriod": "60s",
+                      "perSeriesAligner": "ALIGN_NONE"
+                    }
+                  }
                 }
               }
             ],
@@ -48,63 +58,7 @@ resource "google_monitoring_dashboard" "dashboard_grid_layout_demo" {
             }
           }
         },
-        "width": 6
-      },
-      {
-        "height": 4,
-        "widget": {
-          "text": {
-            "content": "Widget 2",
-            "format": "MARKDOWN",
-            "style": {
-              "backgroundColor": "",
-              "fontSize": "FONT_SIZE_UNSPECIFIED",
-              "horizontalAlignment": "H_LEFT",
-              "padding": "PADDING_SIZE_UNSPECIFIED",
-              "textColor": "",
-              "verticalAlignment": "V_TOP"
-            }
-          }
-        },
-        "width": 6,
-        "xPos": 6
-      },
-      {
-        "height": 4,
-        "widget": {
-          "title": "Widget 3",
-          "xyChart": {
-            "chartOptions": {
-              "mode": "COLOR"
-            },
-            "dataSets": [
-              {
-                "breakdowns": [],
-                "dimensions": [],
-                "measures": [],
-                "plotType": "STACKED_BAR",
-                "targetAxis": "Y1",
-                "timeSeriesQuery": {
-                  "timeSeriesFilter": {
-                    "aggregation": {
-                      "perSeriesAligner": "ALIGN_RATE"
-                    },
-                    "filter": "metric.type=\"agent.googleapis.com/nginx/connections/accepted_count\""
-                  },
-                  "unitOverride": "1"
-                }
-              }
-            ],
-            "thresholds": [],
-            "timeshiftDuration": "0s",
-            "yAxis": {
-              "label": "y1Axis",
-              "scale": "LINEAR"
-            }
-          }
-        },
-        "width": 6,
-        "yPos": 4
+        "width": 2
       }
     ]
   }
