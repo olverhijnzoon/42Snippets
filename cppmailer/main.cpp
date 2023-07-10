30627a441ed121185baecd68049e9386f4ea474c
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-07-11 00:51:57 +0200

    Improve on cppmailer

--- cppmailer/main.cpp
+++ cppmailer/main.cpp
@@ -3,12 +3,30 @@
 #include <unistd.h>
 #include <iostream>
 
+/*
+    This function will be used as a callback for libcurl's CURLOPT_READFUNCTION option. The parameters are as follows:
+
+    void* ptr       ..  A buffer to which the data should be written.
+    size_t size     ..  Size of each data unit.
+    size_t nmemb    ..  Number of units to transfer. The total size to be written is size * nmemb.
+    void* userp     ..  Custom pointer set by CURLOPT_READDATA to pass data to the function.
+*/
 size_t ReadCallback(void* ptr, size_t size, size_t nmemb, void* userp) {
+
+    // This line takes the userp pointer, which is a void pointer, and casts it to a std::string* pointer, which means it's now pointing to a std::string object. We can use s to access and manipulate the string object in this function.
     std::string* s = static_cast<std::string*>(userp);
+
+    // This checks if the buffer size (size * nmemb) is less than 1, which means there's no room to write data, or if the string s is empty. If either condition is true, the function returns 0 to indicate that no data was written.
     if (size * nmemb < 1 || s->empty())
         return 0;
+
+    // This line takes the first character from the string s (retrieved by s->front()) and puts it in the buffer ptr. The ptr pointer is first cast to a char* pointer, then dereferenced to store the character. Since the size of a char is 1 byte, this operation is safe as long as size * nmemb >= 1.
     *(static_cast<char*>(ptr)) = s->front();
+
+    // This line removes the first character from the string s that we've just written to the buffer as it is supposed to "consume" the data it reads.
     s->erase(0, 1);
+
+    // Finally, the function returns 1 to indicate that 1 unit of data was written. In this case, 1 unit corresponds to 1 character. This return value is important, as it tells libcurl how much data was transferred so it can update its counters and continue with the remaining data.
     return 1;
 }
 
