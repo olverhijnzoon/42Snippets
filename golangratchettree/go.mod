1a1191b803a55feb7913d7695788d99e68aa5561
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-07-30 22:26:53 +0200

    Merge state

--- golangratchettree/go.mod
+++ golangratchettree/go.mod
@@ -1,3 +1,3 @@
 module github.com/olverhijnzoon/42Snippets/golangratchettree
 
-go 1.19
+go 1.20
--- golangratchettree/golangratchettree.go
+++ golangratchettree/golangratchettree.go
@@ -7,17 +7,22 @@ import (
 )
 
 type Node struct {
-	Left       *Node
-	Right      *Node
-	Parent     *Node
-	IsLeaf     bool
-	NodeData   string
-	PrivateKey *rsa.PrivateKey
-	PublicKey  rsa.PublicKey
+	Left           *Node
+	Right          *Node
+	Parent         *Node
+	IsLeaf         bool
+	NodeData       string
+	PrivateKey     *rsa.PrivateKey
+	PublicKey      rsa.PublicKey
+	Credential     string // Only for leaf nodes
+	UnmergedLeaves []int  // An ordered list of "unmerged" leaves
+	ParentHash     string // A hash of certain information
+	Index          int
 }
 
 type RatchetTree struct {
-	Root *Node
+	Root      *Node
+	LeafCount int
 }
 
 func (t *RatchetTree) insert(data string, node *Node) *Node {
@@ -29,18 +34,21 @@ func (t *RatchetTree) insert(data string, node *Node) *Node {
 	publicKey := &privateKey.PublicKey
 
 	if t.Root == nil {
-		t.Root = &Node{NodeData: data, IsLeaf: true, PrivateKey: privateKey, PublicKey: *publicKey}
+		t.Root = &Node{NodeData: data, IsLeaf: true, PrivateKey: privateKey, PublicKey: *publicKey, Index: t.LeafCount}
+		t.LeafCount++
 		return t.Root
 	}
 
 	if node.IsLeaf {
 		node.IsLeaf = false
-		node.Left = &Node{Parent: node, NodeData: data, IsLeaf: true, PrivateKey: privateKey, PublicKey: *publicKey}
+		node.Left = &Node{Parent: node, NodeData: data, IsLeaf: true, PrivateKey: privateKey, PublicKey: *publicKey, Index: t.LeafCount}
+		t.LeafCount++
 		return node.Left
 	}
 
 	if node.Right == nil {
-		node.Right = &Node{Parent: node, NodeData: data, IsLeaf: true, PrivateKey: privateKey, PublicKey: *publicKey}
+		node.Right = &Node{Parent: node, NodeData: data, IsLeaf: true, PrivateKey: privateKey, PublicKey: *publicKey, Index: t.LeafCount}
+		t.LeafCount++
 		return node.Right
 	}
 
@@ -49,6 +57,33 @@ func (t *RatchetTree) insert(data string, node *Node) *Node {
 	} else {
 		return t.insert(data, node.Left)
 	}
+}
+
+func (t *RatchetTree) markUnmerged(leafIndex int, ancestorIndex int) {
+	leaf := t.findNodeByIndex(leafIndex)
+	if leaf == nil {
+		fmt.Println("Leaf node not found")
+		return
+	}
+	leaf.UnmergedLeaves = append(leaf.UnmergedLeaves, ancestorIndex)
+}
+
+func (t *RatchetTree) findNodeByIndex(index int) *Node {
+	return t.findNodeByIndexRecursive(t.Root, index)
+}
+
+func (t *RatchetTree) findNodeByIndexRecursive(node *Node, index int) *Node {
+	if node == nil {
+		return nil
+	}
+	if node.Index == index {
+		return node
+	}
+	foundNode := t.findNodeByIndexRecursive(node.Left, index)
+	if foundNode != nil {
+		return foundNode
+	}
+	return t.findNodeByIndexRecursive(node.Right, index)
 }
 
 func (t *RatchetTree) height(node *Node) int {
@@ -87,6 +122,15 @@ func main() {
 
 	for _, name := range names {
 		tree.insert(name, tree.Root)
+	}
+
+	// Mark the leaf node at index 5 as "unmerged" relative to its ancestor at index 2
+	tree.markUnmerged(6, 2)
+
+	// Find the node at index 5 and print its data
+	node := tree.findNodeByIndex(6)
+	if node != nil {
+		fmt.Println("Found node:", node.NodeData)
 	}
 
 	tree.printTree(tree.Root, 0)
