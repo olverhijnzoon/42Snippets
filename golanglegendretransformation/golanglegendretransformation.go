c215d35554f25c8d5dd487d99890c0a010bb18b1
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-07-31 19:16:18 +0200

    Added some heat to the system

--- golanglegendretransformation/golanglegendretransformation.go
+++ golanglegendretransformation/golanglegendretransformation.go
@@ -29,7 +29,7 @@ func main() {
 
 		The Legendre transformation is particularly useful in thermodynamic problems involving processes at nearly constant pressure, as it allows for a change in perspective from internal energy to enthalpy, making the mathematical description of these processes more straightforward. An example illustrating this concept is a gas enclosed in a cylinder with a movable piston (Kolben). As the gas is heated and expands, it does work against the external pressure to push the piston, effectively "making room" for itself. This work, also known as pressure-volume work, is included in the calculation of enthalpy, which is particularly useful for describing systems undergoing changes at nearly constant pressure.
 
-		When the system absorbs heat from its surroundings, its enthalpy increases (ΔH > 0), and the process is said to be endothermic. On the other hand, when the system releases heat to its surroundings, its enthalpy decreases (ΔH < 0), and the process is said to be exothermic.
+		When the system absorbs heat from its surroundings, its enthalpy increases (ΔH > 0), and the process is said to be endothermic. On the other hand, when the system releases heat to its surroundings, its enthalpy decreases (ΔH < 0), and the process is said to be exothermic.Remember, for isobaric (constant pressure) conditions, the change in enthalpy ΔH directly equals the heat absorbed by the system, i.e., ΔH = q (where q is heat).
 	*/
 
 	// define some thermodynamic variables
@@ -41,8 +41,8 @@ func main() {
 		Pressure:       1.0, // bar
 	}
 
-	// perform Legendre transformation
-	result := LegendreTransform(v)
+	// Perform Legendre transformation
+	initialEnthalpy := LegendreTransform(v)
 
 	fmt.Printf("Given Thermodynamic Variables: \n")
 	fmt.Printf("Entropy (S): %.2f kJ/K\n", v.Entropy)
@@ -51,5 +51,18 @@ func main() {
 	fmt.Printf("Volume (V): %.2f m^3\n", v.Volume)
 	fmt.Printf("Pressure (P): %.2f bar\n", v.Pressure)
 	fmt.Printf("\nApplying Legendre transformation from internal energy (U) to enthalpy (H).\n")
-	fmt.Printf("The result of the transformation (H = U + PV) is: %.2f kJ\n", result)
+	fmt.Printf("The initial enthalpy of the system (H) is: %.2f kJ\n", initialEnthalpy)
+
+	// Add heat to the system
+	const Cp float64 = 1.0    // Specific heat at constant pressure in kJ/(kg*K)
+	const deltaT float64 = 50 // Increase in temperature in K
+	q := Cp * deltaT          // Heat added in kJ
+	v.InternalEnergy += q     // Update internal energy
+
+	// Perform Legendre transformation again after adding heat
+	finalEnthalpy := LegendreTransform(v)
+
+	fmt.Printf("\nHeat added to the system (q = Cp*ΔT): %.2f kJ\n", q)
+	fmt.Printf("The final enthalpy of the system after adding heat is: %.2f kJ\n", finalEnthalpy)
+	fmt.Printf("The change in enthalpy ΔH (which equals heat added at constant pressure) is: %.2f kJ\n", finalEnthalpy-initialEnthalpy)
 }
