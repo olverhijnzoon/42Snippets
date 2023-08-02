30dfc47d5dae8d4fc0860777958cc8cd09d6c324
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-08-02 21:04:38 +0200

    Fixing print

--- pythondatastructures/main.py
+++ pythondatastructures/main.py
@@ -38,7 +38,12 @@ nested_structure = {
     'dict_inside_dict': {'key': 'value'},
     'set_inside_dict': {6, 7, 8}
 }
-print("Nested Data Structures:", nested_structure)
+print("Nested Data Structures:")
+print("  List inside dict:", nested_structure['list_inside_dict'])
+print("  Tuple inside dict:", nested_structure['tuple_inside_dict'])
+print("  Dict inside dict:", nested_structure['dict_inside_dict'])
+print("  Set inside dict:", nested_structure['set_inside_dict'])
+
 
 # List Comprehensions
 squares = [x**2 for x in range(10)]
@@ -47,18 +52,18 @@ print("List Comprehensions:", squares)
 # Deque
 from collections import deque
 d = deque([1, 2, 3])
-print("Deque:", d)
+print("Deque:", list(d))
 
 # Counter
 from collections import Counter
 counts = Counter(['a', 'b', 'a', 'c', 'b', 'b'])
-print("Counter:", counts)
+print("Counter:", dict(counts))
 
 # NamedTuple
 from collections import namedtuple
 Point = namedtuple('Point', ['x', 'y'])
 p = Point(11, y=22)
-print("NamedTuple:", p)
+print("NamedTuple:", tuple(p))
 
 # DefaultDict
 from collections import defaultdict
@@ -76,7 +81,7 @@ print("Heap:", heap)
 # OrderedDict
 from collections import OrderedDict
 od = OrderedDict(a=1, b=2, c=3, d=4, e=5)
-print("OrderedDict:", od)
+print("OrderedDict:", dict(od))
 
 # ChainMap
 from collections import ChainMap
@@ -87,7 +92,7 @@ print("ChainMap:", dict(combined))
 
 # Frozenset
 fs = frozenset([1, 2, 3, 2, 1])
-print("Frozenset:", fs)
+print("Frozenset:", set(fs))
 
 # Array
 from array import array
