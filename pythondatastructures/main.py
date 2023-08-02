9ebfb407336e8518c3b94e146bf22191dbd6d144
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-08-02 20:55:31 +0200

    Datastructures everywhere

--- pythondatastructures/main.py
+++ pythondatastructures/main.py
@@ -1,11 +1,14 @@
 # List: Ordered, mutable collection
 fruits = ['apple', 'banana', 'cherry']
+print("List:", fruits)
 
 # Tuple: Ordered, immutable collection
 coordinates = (4, 5)
+print("Tuple:", coordinates)
 
 # Set: Unordered collection with no duplicate elements
 unique_numbers = {1, 2, 3, 2, 1}
+print("Set:", unique_numbers)
 
 # Dictionary: Unordered collection of key-value pairs
 student = {
@@ -13,6 +16,7 @@ student = {
     'age': 20,
     'courses': ['math', 'physics']
 }
+print("Dictionary:", student)
 
 # Custom Class
 class Car:
@@ -34,10 +38,57 @@ nested_structure = {
     'dict_inside_dict': {'key': 'value'},
     'set_inside_dict': {6, 7, 8}
 }
+print("Nested Data Structures:", nested_structure)
 
-# Accessing elements
-print(fruits[0])  # Output: apple
-print(coordinates[1])  # Output: 5
-print(unique_numbers)  # Output: {1, 2, 3}
-print(student['name'])  # Output: John
-print(nested_structure['list_inside_dict'][2])  # Output: 3
+# List Comprehensions
+squares = [x**2 for x in range(10)]
+print("List Comprehensions:", squares)
+
+# Deque
+from collections import deque
+d = deque([1, 2, 3])
+print("Deque:", d)
+
+# Counter
+from collections import Counter
+counts = Counter(['a', 'b', 'a', 'c', 'b', 'b'])
+print("Counter:", counts)
+
+# NamedTuple
+from collections import namedtuple
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(11, y=22)
+print("NamedTuple:", p)
+
+# DefaultDict
+from collections import defaultdict
+dd = defaultdict(int)
+dd['key'] += 1
+print("DefaultDict:", dd)
+
+# Heapq
+import heapq
+heap = [3, 1, 4, 1, 5, 9, 2]
+heapq.heapify(heap)
+print("Heap:", heap)
+
+# OrderedDict
+from collections import OrderedDict
+od = OrderedDict.fromkeys('abcde')
+print("OrderedDict:", od)
+
+# ChainMap
+from collections import ChainMap
+baseline = {'a': 1, 'b': 2}
+adjustments = {'b': 3, 'c': 4}
+combined = ChainMap(adjustments, baseline)
+print("ChainMap:", combined)
+
+# Frozenset
+fs = frozenset([1, 2, 3, 2, 1])
+print("Frozenset:", fs)
+
+# Array
+from array import array
+arr = array('I', [1, 2, 3, 4])
+print("Array:", arr)
