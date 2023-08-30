455a0c066c512cbd8b7f0be334f405e98e222182
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-08-30 21:24:40 +0200

    Quick fix to C++20

--- cpphiggs/electroweak_simulation.cpp
+++ cpphiggs/electroweak_simulation.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 // Higgs Field class
 class HiggsField {
@@ -13,41 +14,63 @@ public:
     }
 };
 
-// Particle class
+// Base Particle class
 class Particle {
-private:
+protected:
     std::string name;
     double mass{0.0};
 
 public:
     explicit Particle(std::string n) : name(std::move(n)) {}
+    virtual void interactWithHiggs(const HiggsField& higgs) = 0;
+    virtual void display() const {
+        std::cout << "Particle: " << name << ", Mass: " << mass << " GeV" << std::endl;
+    }
+};
 
-    void interactWithHiggs(const HiggsField& higgs) {
+// Bosons
+class Boson : public Particle {
+public:
+    using Particle::Particle;
+    void interactWithHiggs(const HiggsField& higgs) override {
         if (name == "W" || name == "Z") {
             mass = higgs.getVacuumExpectationValue();  // Simplified representation
         }
         // photon remains massless by default
     }
+};
 
-    void display() const {
-        std::cout << "Particle: " << name << ", Mass: " << mass << " GeV" << std::endl;
+// Fermions
+class Fermion : public Particle {
+public:
+    using Particle::Particle;
+    void interactWithHiggs(const HiggsField& higgs) override {
+        if (name == "electron") {
+            mass = 0.511;  // MeV, simplified representation
+        } else if (name == "neutrino") {
+            mass = 0.00001;  // MeV, very tiny mass
+        } else if (name == "up quark" || name == "down quark") {
+            mass = 2.3;  // MeV for up quark, simplified representation
+        }
     }
 };
 
 int main() {
     auto higgs = std::make_unique<HiggsField>();
 
-    auto W = std::make_unique<Particle>("W");
-    auto Z = std::make_unique<Particle>("Z");
-    auto photon = std::make_unique<Particle>("photon");
+    std::vector<std::unique_ptr<Particle>> particles;
+    particles.push_back(std::make_unique<Boson>("W"));
+    particles.push_back(std::make_unique<Boson>("Z"));
+    particles.push_back(std::make_unique<Boson>("photon"));
+    particles.push_back(std::make_unique<Fermion>("electron"));
+    particles.push_back(std::make_unique<Fermion>("neutrino"));
+    particles.push_back(std::make_unique<Fermion>("up quark"));
+    particles.push_back(std::make_unique<Fermion>("down quark"));
 
-    W->interactWithHiggs(*higgs);
-    Z->interactWithHiggs(*higgs);
-    photon->interactWithHiggs(*higgs);
-
-    W->display();
-    Z->display();
-    photon->display();
+    for (const auto& particle : particles) {
+        particle->interactWithHiggs(*higgs);
+        particle->display();
+    }
 
     return 0;
 }
