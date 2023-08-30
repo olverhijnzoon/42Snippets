5977efd2cc8cdfec45cde363d0918b999e432121
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-08-30 21:46:35 +0200

    Even more beauty

--- cpphiggs/higgs_field.cpp
+++ cpphiggs/higgs_field.cpp
@@ -1,10 +1,12 @@
 #include "higgs_field.h"
 #include <cmath>
 
+// Constructor for the HiggsField class
 HiggsField::HiggsField(double mu2, double lambda) : mu2(mu2), lambda(lambda), vacuumExpectationValue(0.0) {
     calculateVEV();
 }
 
+// Method to calculate the Vacuum Expectation Value (VEV) based on the Higgs potential
 void HiggsField::calculateVEV() {
     // VEV v = sqrt(-μ^2 / (2λ))
     if (lambda != 0.0) {
@@ -12,6 +14,7 @@ void HiggsField::calculateVEV() {
     }
 }
 
+// Getter method to retrieve the calculated VEV
 double HiggsField::getVacuumExpectationValue() const {
     return vacuumExpectationValue;
 }
