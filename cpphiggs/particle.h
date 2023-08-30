7194d893e3868ad6a5c6023947b4f378ca3d4fe6
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-08-30 22:49:19 +0200

    Particle

--- cpphiggs/particle.h
+++ cpphiggs/particle.h
@@ -1,12 +1,21 @@
-#pragma once
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
 #include <string>
 #include <memory>
 #include "higgs_field.h"
 
-// Forward declaration
-class ParticleInteraction;
-
-// Abstract base class representing a generic particle
+/**
+ * @class Particle
+ * 
+ * @brief Abstract base class representing a generic particle in the Standard Model of particle physics.
+ * 
+ * The Particle class provides a foundation for modeling different types of subatomic particles,
+ * such as bosons and fermions. It defines common attributes like name and mass and provides
+ * virtual methods for interactions with the Higgs field and other particles.
+ * 
+ * Derived classes should provide specific implementations for these interactions.
+ */
 class Particle {
 protected:
     std::string name;  // Name of the particle
@@ -16,17 +25,52 @@ public:
     explicit Particle(std::string n);
     virtual ~Particle() = default;
 
+    /**
+     * @brief Interact with the Higgs field.
+     * 
+     * This virtual method should be overridden by derived classes to provide specific
+     * implementations for how different particles interact with the Higgs field.
+     * 
+     * @param higgs The Higgs field with which the particle interacts.
+     */
     virtual void interactWithHiggs(const HiggsField& higgs) = 0;
-    virtual std::unique_ptr<ParticleInteraction> interact() = 0;  // New interaction method
+
+    /**
+     * @brief Simulate the interaction of the particle with other particles.
+     * 
+     * This virtual method should be overridden by derived classes to provide specific
+     * descriptions of particle interactions.
+     * 
+     * @return A unique pointer to a ParticleInteraction object describing the interaction.
+     */
+    virtual std::unique_ptr<ParticleInteraction> interact() = 0;
+
+    /**
+     * @brief Display the particle's name and mass.
+     */
     virtual void display() const;
 };
 
-// Class representing a particle interaction
+/**
+ * @class ParticleInteraction
+ * 
+ * @brief Represents the interaction of a particle with other particles.
+ * 
+ * The ParticleInteraction class provides a way to model and describe the interactions
+ * between different particles. For instance, an up quark might change to a down quark
+ * while emitting a W+ boson. This class encapsulates such interactions.
+ */
 class ParticleInteraction {
 private:
-    std::string description;
+    std::string description;  // Description of the interaction
 
 public:
     explicit ParticleInteraction(std::string desc);
+
+    /**
+     * @brief Display the description of the particle interaction.
+     */
     void display() const;
 };
+
+#endif // PARTICLE_H
